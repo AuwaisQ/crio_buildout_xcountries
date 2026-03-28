a4e17561da27997326127253bbc72b2f1889e093
auwaisqureshi <auwaisqureshi@gmail.com>
2026-03-28 18:02:01 +0530

    module completed

--- src/App.js
+++ src/App.js
@@ -1,23 +1,29 @@
-import logo from './logo.svg';
+import { useState, useEffect } from 'react';
 import './App.css';
 
 function App() {
+
+  const [countries, setCountries] = useState([]);
+
+  useEffect(() => {
+    fetch('https://xcountries-backend.labs.crio.do/all')
+      .then((res) => res.json())
+      .then((data) => setCountries(data));
+  }, []);
+
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {countries.length > 0 ? (
+        countries.map((country) => (
+          <div className="country-card" key={country.name}>
+            <img src={country.flag} alt={`${country.name} flag`}  height='100px' width='100px' />
+            <h2>{country.name}</h2>
+          </div>
+        ))
+      ) : (
+        <p>Loading...</p>
+      )}
     </div>
   );
 }
